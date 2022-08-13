142c4bd939e9b373015b2979ba1a5efbcf759251
ProwlingLynx <tamezjavier@gmail.com>
2022-08-12 23:29:18 -0500

    Adding axios to server dir. Added logic to route for proof connection to GAS.

--- server/src/routes/get.ts
+++ server/src/routes/get.ts
@@ -1,8 +1,21 @@
 import express from "express";
+import {makeGetOptions} from "../utils";
+import axios from "axios";
 
 export const getRouter = express.Router();
 
 getRouter.get("/cat", (req : express.Request, res : express.Response): void => {
   console.log("The cat meowed!");
   res.send("Meow");
+});
+
+getRouter.get("/fakeGas", async (req : express.Request, res : express.Response): Promise<void> => {
+  const options = makeGetOptions("something fakeName", "something@fakeMail.com");
+  try {
+    const {data} = await axios(options);
+    console.log("this was data: ", data);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+  }
 });
