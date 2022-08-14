fc4d959f9bc537840b868fa42d2c1d11ed98a62e
ProwlingLynx <tamezjavier@gmail.com>
2022-08-14 13:36:55 -0500

    Adding conditional logic for developement/testing variables. Removed old comments.

--- sbsm-admin-app/src/components/GoogleSignIn.tsx
+++ sbsm-admin-app/src/components/GoogleSignIn.tsx
@@ -1,42 +1,44 @@
 import { useEffect } from "react"
 
 export const GoogleSignIn = () => {
-  // console.log("GoogleSignIn: ", process.env.REACT_APP_CLIENT_ID);
 
   useEffect(() => {
+    async function handleCredentialResponse(response: any) {
+      try {
 
-    async function handleCredentialResponse(response : any) {
-    try{
+        let url = window.location.origin + "/verify/user";
+        if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
+          url = "http://localhost:4001/verify/user";
+          console.log("Encoded JWT ID token: " + response.credential);
+        }
+        const res = await fetch(url, {
+          method: "POST",
+          headers: {
+            "authorization": "Bearer " + response.credential,
+          },
+        })
+        console.log("Response: " + res);
+      } catch (error) {
+        console.log(error);
+      };
+    }
+    google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_CLIENT_ID,
+      callback: handleCredentialResponse
+    });
 
-      console.log("Encoded JWT ID token: " + response.credential);
-      // fetch(window.location.origin+"/verify/user", {
-      fetch("http://localhost:4001/verify/user", {
-        method: "POST",
-        headers: {
-          "authorization": "Bearer " + response.credential,
-        },
-      })
-    } catch(error) {
-      console.log(error);
-    };
-  }
-  google.accounts.id.initialize({
-    client_id: process.env.REACT_APP_CLIENT_ID,
-    callback: handleCredentialResponse
-  });
+    //If you prefer the button then uncomment the function below and comment out the pormpt method instead.
 
-  //If you prefer the button then uncomment the function below and comment out the pormpt method instead.
+    // google.accounts.id.renderButton(
+    //   document.getElementById("buttonDiv"),
+    //   { theme: "outline", size: "large" }  // customization attributes
+    // );
 
-  // google.accounts.id.renderButton(
-  //   document.getElementById("buttonDiv"),
-  //   { theme: "outline", size: "large" }  // customization attributes
-  // );
-
-  google.accounts.id.prompt(); // also display the One Tap dialog
+    google.accounts.id.prompt(); // also display the One Tap dialog
   }, []);
 
 
-  return(
+  return (
     <div>
       To proceed forward, please sign in with Google. Otherwise, you need to refresh the page.
     </div>
