e1aecf5535cb3186415d577a4d89ed2e6b36f68b
ProwlingLynx <tamezjavier@gmail.com>
2022-08-14 14:45:28 -0500

    Extracted testing middleware to utils.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,32 +1,21 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import {getRouter, postRouter} from "./routes"
-// import getRouter from "./routes/get"
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { getRouter, postRouter } from './routes';
+import { allowCors } from './utils';
 
-dotenv.config({ path: ".env" });
+dotenv.config({ path: '.env' });
 const app = express();
 
-//The following middleware is used for Development purposes only.
-//It allows us to use get requests from react-app.
-app.use((req, res, next) => {
-  const isOptions = req.method.toLocaleLowerCase() === "options";
-  if (isOptions) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Methods", "post");
-    // res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Authorization");
-    res.sendStatus(200);
-    return;
-  }
-  console.log(`Current request: ${req.method} \nat url: ${req.url}`);
-  next();
-})
+if (process.env.MODE === 'development') {
+  //The following middleware is used for Development purposes only.
+  //It allows us to use get requests from react-app.
+  app.use(allowCors);
+}
 
-app.use(express.static('../sbsm-admin-app/build'))
+app.use(express.static('../sbsm-admin-app/build'));
 app.use('/', getRouter);
 app.use('/', postRouter);
 
-
-app.listen(process.env.PORT, () : void => {
+app.listen(process.env.PORT, (): void => {
   console.log(`Server listening on port: ${process.env.PORT}`);
 });
--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -1,6 +1,7 @@
-import type {GetOptions} from './types';
+import type { GetOptions, ExtendRequests, ExtendResponse } from './types';
 
-export const makeGetOptions : (name : string, email : string) => GetOptions = (name, email) => {
+export const makeGetOptions: (name: string, email: string) => GetOptions =
+  (name, email) => {
   if (typeof name !== 'string' || typeof email !== 'string') throw new Error('Invalid arguments');
   const options = {
     method: 'get',
@@ -10,7 +11,26 @@ export const makeGetOptions : (name : string, email : string) => GetOptions = (n
       secret: process.env.API_SECRET,
       name,
       email,
-    }
+    },
   };
   return options;
-}
+};
+
+export const allowCors: (req: ExtendRequests, res: ExtendResponse, next: Function) => void = (
+  req,
+  res,
+  next
+) => {
+  const isOptions = req.method.toLocaleLowerCase() === 'options';
+  if (isOptions) {
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'post');
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+    // res.header('Access-Control-Allow-Headers', 'Authorization');
+    res.sendStatus(200);
+    return;
+  }
+  console.log(`Current request: ${req.method} \nat url: ${req.url}`);
+  next();
+};
