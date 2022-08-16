25d215d3798e57f57352d54c2315846c6b265271
ProwlingLynx <tamezjavier@gmail.com>
2022-08-16 01:12:49 -0500

    Adding error handling for switch cases. Adding error message displays.

--- sbsm-admin-app/src/App.tsx
+++ sbsm-admin-app/src/App.tsx
@@ -1,15 +1,27 @@
 import './App.css';
 import { useState } from 'react';
-import { Login } from './pages';
+import { Login, StudentList, ErrorPage } from './pages';
 import { googleSignIn } from './components';
 function App() {
   const [location, setLocation] = useState('login');
+  /*
+  currently using:
+  - "student list"
+  */
   googleSignIn.setLocationChanger(setLocation);
-  switch(location) {
-    case 'login':
-      return (<Login location={location} setLocation={setLocation}/>);
-    default:
-      return (<div>404</div>);
+  try {
+    switch(location) {
+      case 'login':
+        return (<Login location={location} setLocation={setLocation}/>);
+      case 'student list':
+        return (<StudentList location={location} setLocation={setLocation}/>);
+      default:
+        throw new Error(`Unknown location: ${location}`);
+    }
+
+  } catch (error) {
+    console.log(error);
+    return (<ErrorPage message={JSON.stringify(error)}/>)
   }
 };
 
--- sbsm-admin-app/src/pages/ErrorPage.tsx
+++ sbsm-admin-app/src/pages/ErrorPage.tsx
@@ -1,1 +1,8 @@
-export {};
+export const ErrorPage = ({message}: {message: string}) => {
+  return (
+    <div className="error-page">
+      <h1>Error</h1>
+      <p>{message}</p>
+    </div>
+  );
+};
