c5be3d0f31de4ac36caf6ad6f2a79835c4e51252
ProwlingLynx <tamezjavier@gmail.com>
2022-08-17 10:24:49 -0500

    Implemented route for GAS scripts. Added google auth logic.

--- server/src/routes/post.ts
+++ server/src/routes/post.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
+import { getGas } from "./gas";
+import { verify } from "./google";
 
 export const postRouter = Router();
 
-postRouter.post("/verify/user", (req, res) => {
-  console.log("verify user called: ", req.get("authorization"));
-  console.log("Verifying user: ", req.header);
-  res.send("verify user called").status(200);
+postRouter.post("/verify/user", async (req, res) => {
+  try{
+    console.log("verify user called: ", req.get("authorization"));
+    console.log("Verifying user: ", req.header);
+    const user = await verify(req.get("authorization") as string);
+    console.log(user);
+    const data = await getGas(user);
+    res.send(data).status(200);
+  } catch (error) {
+    console.log(error);
+    res.status(410).send("Invalid Token");
+  }
 });
